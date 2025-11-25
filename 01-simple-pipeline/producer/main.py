cc39de43dbc51b62103735ccd4f45b24b34dd50d
Parham Abbasi <prmbas@gmail.com>
2025-11-25 13:21:34 +0330

    CLI args for custom messages

--- 01-simple-pipeline/producer/main.py
+++ 01-simple-pipeline/producer/main.py
@@ -1,14 +1,18 @@
 from kafka import KafkaProducer
 import os, sys
 
-producer = KafkaProducer(
-    bootstrap_servers=f'{os.getenv("KAFKA_ADDRESS")}:{os.getenv("KAFKA_PORT")}',
-    value_serializer=lambda v: v.encode('utf-8')
-)
+if __name__ == '__main__':
+    try:
+        msg, num = sys.argv[1], int(sys.argv[2])
+    except IndexError:
+        msg, num = f'hello', 10
+    producer = KafkaProducer(
+        bootstrap_servers=f'{os.getenv("KAFKA_ADDRESS")}:{os.getenv("KAFKA_PORT")}',
+        value_serializer=lambda v: v.encode('utf-8')
+    )
 
-for i in range(5):
-    print(f"{i}")
-    producer.send(os.getenv("TOPIC_NAME"), f'hello-{i}')
+    for i in range(num):
+        producer.send(os.getenv("TOPIC_NAME"), msg)
 
-producer.flush()
-print("Done")
+    producer.flush()
+    print("Done")
